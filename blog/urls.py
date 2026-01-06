1795bb7aa0d2eccd9b0ad113c3597da41a9d39dd
Amir <yosefiamirmohammas@gmail.com>
2026-01-06 20:22:02 +0330

    Remove deprecated files from the project

--- blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from blog.views import *
-
-app_name = "blog"
-urlpatterns = [
-    path("", index, name="home"),
-    path("single", single_blog, name="single"),
-]
--- website/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import index, about, contact
-
-app_name = "website"
-urlpatterns = [
-    path("", index, name="home"),
-    path("about/", about, name="about"),
-    path("contact/", contact, name="contact"),
-]
